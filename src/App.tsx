3a3f35735a3f140dc3f4c580bab51cb8eefc4e69
hsu <viewsonic521@gmail.com>
2023-09-11 17:18:04 +0900

    update all components with data to build interactive graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import useResasContext from './hooks/use-resas-context';
+import Checkbox from './components/Checkbox';
+import Graph from './components/Graph';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { fetchPrefectures, prefecturesState, fetchPopulation } = useResasContext();
+	useEffect(() => {
+		fetchPrefectures();
+	}, [fetchPrefectures]);
+
+	const [checkboxes, setCheckboxes] = useState<{ [key: number]: boolean }>({});
+	const handleCheckboxChange = (prefName: string, prefCode: number, checked: boolean) => {
+		setCheckboxes(prevCheckboxes => ({
+			...prevCheckboxes,
+			[prefCode]: checked,
+		}));
+		if (checked) {
+			fetchPopulation(prefName, prefCode);
+		}
+	};
+	return (
+		<>
+			<header>
+				<h1>都道府県別の総人口推移グラフ</h1>
+			</header>
+			<main>
+				<div className='prefectures-container'>
+					<h3>都道府県</h3>
+					<div className='prefectures'>
+						{prefecturesState.map(prefecture => (
+							<Checkbox
+								key={prefecture.prefCode}
+								prefCode={prefecture.prefCode}
+								prefName={prefecture.prefName}
+								checked={checkboxes[prefecture.prefCode] || false}
+								checkChanged={handleCheckboxChange}
+							/>
+						))}
+					</div>
+				</div>
+				<Graph />
+			</main>
+		</>
+	);
 }
 
 export default App;
